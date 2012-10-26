ecd22d0b9983d18ba804978a31d2e279ee8da72f
Anthony Shortland <anthony@dtosolutions.com>
2012-10-26 11:49:07 -0700

    Fixes #5: Fixed up build-1-test.sh to build jboss-as-7.1.1.Final using the JBoss build example.

--- tests/build-1-test.sh
+++ tests/build-1-test.sh
@@ -18,6 +18,22 @@ rerun() {
 
 describe "build"
 
-it_runs_without_arguments() {
-    rerun rpm:build
+it_can_build_the_jboss_example() {
+   # make sure the distribution is available:
+   if [[ -e $RERUN_MODULES/rpm/examples/build/jboss/SOURCES/jboss-as-7.1.1.Final.zip ]]
+   then
+      echo "\"rpm/examples/build/jboss/SOURCES/jboss-as-7.1.1.Final.zip\" has already been downloaded"
+   else
+      echo "Downloading \"rpm/examples/build/jboss/SOURCES/jboss-as-7.1.1.Final.zip\" from jboss.org ..."
+      cd $RERUN_MODULES/rpm/examples/build/jboss/SOURCES
+      wget -qO $RERUN_MODULES/rpm/examples/build/jboss/SOURCES/jboss-as-7.1.1.Final.zip http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip
+   fi
+
+   # build the package:
+   rerun rpm:build -n jboss-as -v 7.1.1.Final -t $RERUN_MODULES/rpm/examples/build/jboss
+
+   # check the package:
+   rpm -qi -p $RERUN_MODULES/rpm/examples/build/jboss/RPMS/noarch/jboss-as-7.1.1.Final-1.noarch.rpm
+   rpm -qlv -p $RERUN_MODULES/rpm/examples/build/jboss/RPMS/noarch/jboss-as-7.1.1.Final-1.noarch.rpm
+   rpm -q --requires -p $RERUN_MODULES/rpm/examples/build/jboss/RPMS/noarch/jboss-as-7.1.1.Final-1.noarch.rpm
 }
