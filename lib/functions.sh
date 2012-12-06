19eb593d5db2fd721637fdce9fbc480a52d23291
Anthony Shortland <anthony@dtosolutions.com>
2012-12-06 15:05:49 -0800

    Fixes #11 by updating existing commands to work with the latest Rerun.

--- lib/functions.sh
+++ lib/functions.sh
@@ -1,7 +1,6 @@
-# 
-# Shell functions for rpm commands
+# Shell functions for the rpm module.
+#/ usage: source RERUN_MODULE_DIR/lib/functions.sh command
 #
-
 
 # Read rerun's public functions
 . $RERUN || {
@@ -9,9 +8,19 @@
     return 1
 }
 
+# Check usage. Argument should be command name.
+[[ $# = 1 ]] || rerun_option_usage
 
-# ----------------------------
-# Your functions declared here.
+# Source the option parser script.
 #
+if [[ -r $RERUN_MODULE_DIR/commands/$1/options.sh ]] 
+then
+    . $RERUN_MODULE_DIR/commands/$1/options.sh || {
+        rerun_die "Failed loading options parser."
+    }
+fi
 
+# - - -
+# Your functions declared here.
+# - - -
 
